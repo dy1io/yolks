3d801c9d9155dcf035f88b72c3f1b1a87c98ae43
Inquisitor <16191249+deucex6400@users.noreply.github.com>
2023-07-11 13:13:13 -0500

    Update entrypoint.sh

--- games/dayz/entrypoint.sh
+++ games/dayz/entrypoint.sh
@@ -216,7 +216,8 @@ fi
 # DEBUGGING
 echo -e "\n${PURPLE}[DEBUGGING]: Launcher Tag: ${LAUNCHER_TAG}"
 
-if [[ -f ${MODLIST} ]] && [[ -n "$(cat ${MODLIST} | grep `${LAUNCHER_TAG}`)" ]]; then
+# DayZ Launcher
+if [[ -f ${MODLIST} ]] && [[ -n "$(cat ${MODLIST} | grep 'Created By DayZ Launcher')" ]]; then
     CLIENT_MODS+=$(cat ${MODLIST} | grep 'id=' | cut -d'=' -f3 | cut -d'"' -f1 | xargs printf '@%s;')
 elif [[ -n "${MOD_FILE}" ]]; then # If MOD_FILE is not null, warn user file is missing or invalid
     echo -e "\n${YELLOW}[STARTUP_WARN]: Modlist file \"${CYAN}${MODLIST}${YELLOW}\" could not be found, or is invalid!${NC}"
